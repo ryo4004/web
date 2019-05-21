1009d35613d532d77f85e70e5d9c1bb6b82314a5
ryo4004 <regs.ryo4004@gmail.com>
2019-05-21 12:49:19 +0900

    トップページのヒーローヘッダーを編集

--- html/index.php
+++ html/index.php
@@ -1,12 +1,16 @@
-<html>
+<html lang="ja">
 <head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1.0">
   <title>ザ・ウィンド・アンサンブルへようこそ！</title>
   <link rel="stylesheet" href="css/reset.css">
   <link rel="stylesheet" href="css/index.css">
+  <link rel="shortcut icon" href="image/favicon.ico">
 </head>
 <body>
   <div class='top-background' id='video-frame'>
-    <video id='video' autoload autoplay loop muted controles='false' playsinline>
+    <video id='video' preload='auto' autoplay loop muted playsinline>
       <source src='image/video/top.mp4' type='video/mp4'>
       <source src='image/video/top.webm' type='video/webm'>
     </video>
@@ -15,6 +19,13 @@
         <?php require('image/logo/logo.svg'); ?>
         <?php require('image/logo/logo_mobile.svg'); ?>
         <q id='quote'></q>
+      </div>
+    </div>
+    <div id='loading'>
+      <div class="loading">
+        <div class="loading1"></div>
+        <div class="loading2"></div>
+        <div class="loading3"></div>
       </div>
     </div>
   </div>
--- html/js/index.js
+++ html/js/index.js
@@ -1,21 +1,31 @@
 function videoAdjust () {
+  document.getElementById('video-frame').style.height = window.innerHeight
   const video = document.getElementById('video')
   const videoWidth = video.clientWidth
-  const videoHeight = video.clientWidth
-  console.log(videoWidth, videoHeight)
+  const videoHeight = video.clientHeight
+  // const videoWidth = video.scrollWidth
+  // const videoHeight = video.scrollHeight
+  // const videoWidth = window.innerHeight === video.scrollWidth ? video.scrollWidth : video.clientWidth
+  // const videoHeight = window.innerWidth === video.scrollHeight ? video.scrollHeight : video.clientWidth
   const fix = Math.max(window.innerWidth / videoWidth, window.innerHeight / videoHeight)
   video.style.width = videoWidth * fix
   video.style.height = videoHeight * fix
   video.style.top = Math.floor((window.innerHeight - videoHeight * fix) / 2)
   video.style.left = Math.floor((window.innerWidth - videoWidth * fix) / 2)
-  document.getElementById('video-frame').style.height = window.innerHeight
 }
 function setQuote () {
   const quote = document.getElementById('quote')
   '音楽の輪が限りなく広がりますように'.split('').forEach(each => quote.innerHTML += '<span>' + each + '</span>')
   quote.innerHTML += '<span>&mdash;&mdash;</span>'
 }
+function loadingClose () {
+  document.getElementById('loading').classList.add('hide')
+  setTimeout(() => document.getElementById('loading').classList.add('none'), 200)
+}
 window.addEventListener('load', () => {
+  document.getElementById('video').controls = false
+  if (document.getElementById('video').readyState > 3) loadingClose()
+  document.getElementById('video').addEventListener('canplay', () => loadingClose())
   videoAdjust()
   setQuote()
 })
