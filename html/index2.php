ee3e4770078e85cc333e2e50b2c003f77a212394
ryo4004 <regs.ryo4004@gmail.com>
2019-05-26 08:42:55 +0900

    トップページ案を更新

--- html/index2.php
+++ html/index2.php
@@ -43,35 +43,96 @@
 
   <div>
 
-    <div class='block concert'>
-      <div class='background photo'>
-        <!-- <img src='image/photo/spring.jpg' class='spring'> -->
-        <div class='cover spring'></div>
-        <!-- <img src='image/photo/autumn.jpg' class='autumn'> -->
-        <div class='cover autumn'></div>
-        <!-- <div class='overlay'></div> -->
+    <div class='not-block introduction' id="introduction">
+      <div class='background map'>
+        <?php require('image/svg/japan-niigata3_3.svg'); ?>
       </div>
       <div class='contents'>
         <div>
-          <div class='title'>
-            <h2 class='title' data-subttl="Concert">演奏会</h2>
+          <h2 class='location' data-subttl="About us">私たちについて</h2>
+          <!-- <p>拠点
+            私たちザ・ウィンド・アンサンブル(ウィンズ)は、新潟県長岡市を中心に活動しています。<br />
+            「音楽の輪が限りなく広がりますように」を標語として、年2回開かれる演奏会を通して吹奏楽の楽しさを広めて行きたいと思っております。
+          </p>
+          <p>
+            アマチュアバンドにとって月数回の練習こそ活動目的ですが、その他の主な活動として定期演奏会、ミニコンサートを行っております。<br />
+            定期演奏会では、吹奏楽のオリジナル作品やクラシックのアレンジ作品を、野外コンサートでは、アニメソング、ポップス、映画音楽などを主体に音楽を楽しんでいます。
+          </p>
+          <p>
+            またこれらの演奏会の他に、出張、依頼演奏なども随時受け付けております。
+            お気軽にお問い合わせからご連絡ください。
+          </p> -->
+          <p>ザ・ウィンド・アンサンブル(ウィンズ)は、新潟県長岡市を中心に活動している社会人吹奏楽団です</p>
+          <a href='#' class='button'>More</a>
+        </div>
+      </div>
+    </div>
+
+    <div class='not-block concert'>
+      <!-- <div class='background'> -->
+        <!-- <div class='cover'></div> -->
+        <!-- <img src='image/photo/spring.jpg' class='spring'> -->
+        <!-- <div class='cover spring'></div> -->
+        <!-- <img src='image/photo/autumn.jpg' class='autumn'> -->
+        <!-- <div class='cover autumn'></div> -->
+        <!-- <div class='overlay'></div> -->
+      <!-- </div> -->
+      <div class='contents'>
+        <div>
+          <div class='concert-introduction'>
+            <div class='title'>
+              <h2 class='title' data-subttl="Concert">演奏会</h2>
+            </div>
+            <div class='text'>
+              <p>ザ・ウィンド・アンサンブルは年2回、演奏会を主催しております。</p>
+              <p>これらの演奏会の他に出張依頼演奏も随時受け付けております。</p>
+              <p>お気軽にお問合せからご連絡ください。</p>
+            </div>
           </div>
-          <div class='list'>
-            <p>主に年2回、演奏会を行っております</p>
-            <div class='each-concert'>
-              <a href='#'>
-                <div>
-                  <p>2019年10月13日(日)</p>
-                  <h3>第32回定期演奏会</h3>
+          <div class='concert-box'>
+            <div class='main-concert' style='order:1'>
+              <div class='content'>
+                <h3 class='title' data-subttl="Annual Concert">定期演奏会</h3>
+                <div class='text'>
+                  <p>秋に開催する定期演奏会では、吹奏楽のオリジナル作品やクラシックのアレンジ作品をメインに演奏いたします。</p>
                 </div>
-                <div><i class="fas fa-chevron-right"></i></div>
-              </a>
+                <ul>
+                  <li>
+                    <a href='#' class='open'>
+                      <div class='concert-detail'>
+                        <div><p>2019年10月13日(日)</p><h3>第32回定期演奏会</h3></div>
+                        <div class='link-arrow'><i class="fas fa-chevron-right"></i></div>
+                      </div>
+                      <div class='concert-close'>終了しました</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class='mini-concert' style='order:2'>
+              <div class='content'>
+                <h3 class='title' data-subttl="Mini Concert">ミニコンサート</h3>
+                <div class='text'>
+                  <p>春に開催するミニコンサートでは、アニメソングやポップス、映画音楽などをメインに演奏いたします。</p>
+                </div>
+                <ul>
+                  <li>
+                    <a href='#' class='close'>
+                      <div class='concert-detail'>
+                        <div><p>2019年5月11日(土)</p><h3>春のミニコンサート</h3></div>
+                        <div class='link-arrow'><i class="fas fa-chevron-right"></i></div>
+                      </div>
+                      <div class='concert-close'>終了しました</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-    
+
     <div class='block schedule'>
       <div class='background'>
         <img src='image/photo/drum.jpg'>
@@ -84,28 +145,6 @@
       </div>
     </div>
 
-    <div class='block introduction' id="introduction">
-      <div class='background map'>
-        <?php require('image/svg/japan-niigata3_3.svg'); ?>
-      </div>
-      <div class='contents'>
-        <h2 class='location' data-subttl="About us">私たちについて</h2>
-        <!-- <p>拠点
-          私たちザ・ウィンド・アンサンブル(ウィンズ)は、新潟県長岡市を中心に活動しています。<br />
-          「音楽の輪が限りなく広がりますように」を標語として、年2回開かれる演奏会を通して吹奏楽の楽しさを広めて行きたいと思っております。
-        </p>
-        <p>
-          アマチュアバンドにとって月数回の練習こそ活動目的ですが、その他の主な活動として定期演奏会、ミニコンサートを行っております。<br />
-          定期演奏会では、吹奏楽のオリジナル作品やクラシックのアレンジ作品を、野外コンサートでは、アニメソング、ポップス、映画音楽などを主体に音楽を楽しんでいます。
-        </p>
-        <p>
-          またこれらの演奏会の他に、出張、依頼演奏なども随時受け付けております。
-          お気軽にお問い合わせからご連絡ください。
-        </p> -->
-        <p>ザ・ウィンド・アンサンブル(ウィンズ)は、新潟県長岡市を中心に活動している社会人吹奏楽団です</p>
-        <a href='#' class='button'>More</a>
-      </div>
-    </div>
   </div>
   <footer>
     <div>
