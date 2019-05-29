17f3272ebf3756e1e502750cf077fef01b522e95
ryo4004 <regs.ryo4004@gmail.com>
2019-05-29 10:54:13 +0900

    個人情報保護方針ページを追加

---
+++ html/policy.php
@@ -0,0 +1,99 @@
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1.0">
+  <title>個人情報保護方針｜ザ・ウィンド・アンサンブル</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/policy.css">
+  <link rel="shortcut icon" href="image/favicon.ico">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+  <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+  <script>
+    ((d) => {
+      var config = { kitId: 'wxw0kpr', scriptTimeout: 3000, async: true },
+      h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document)
+  </script>
+</head>
+<body>
+
+  <header id='top'>
+    <section id="main-logo">
+      <a href='index.php'>
+        <?php require('image/logo/logo.svg'); ?>
+      </a>
+    </section>
+  </header>
+
+  <div class='top-title'>
+    <div>
+      <h1 data-subttl="Privacy Policy">個人情報保護方針</h1>
+      <div class='bread'><a href='index.php'>ホーム</a><i class="fas fa-chevron-right"></i><a href='policy.php'>個人情報保護方針</a></div>
+    </div>
+  </div>
+
+  <div class='block policy' id="policy">
+    <div class='contents'>
+      <div class='text'>
+        <p>ザ・ウィンド・アンサンブル(以下当会)は、以下のとおり個人情報保護方針を定め、個人情報保護の仕組みの構築および管理の徹底に取り組みます。</p>
+        <h2>基本方針</h2>
+        <p>当会は、個人情報の重要性を認識し個人情報を保護することが社会的責務であると考え、個人情報に関する法令を遵守し、取り扱う個人情報の取得、利用、管理を適切に行います。ウェブサイト訪問者およびお客様の個人情報を一元管理することにより、不正アクセス、紛失、破壊、改ざん、漏洩などを防止します。</p>
+        <h2>適用範囲</h2>
+        <p>本プライバシーポリシーは当会で取り扱うお客様の個人情報すべてに適用されます。</p>
+        <h2>個人情報の取得と利用目的</h2>
+        <p>当会が取得する個人情報は下記の通りです。</p>
+        <h3>お問い合わせフォーム</h3>
+        <p>本ウェブサイトでは、お問い合わせフォームを設置しております。ウェブサイト訪問者がお問い合わせフォームに入力した以下の個人情報を取得します。</p>
+        <ul>
+          <li>お名前(ハンドルネーム)</li>
+          <li>メールアドレス</li>
+          <li>お問い合わせ内容</li>
+        </ul>
+        <p>これらの個人情報はお問い合わせに対する回答の際以外では利用いたしません。</p>
+        <h3>Googleアナリティクス</h3>
+        <p>本ウェブサイトでは、アクセス解析のためにGoogleアナリティクスを使用しております。Googleアナリティクスは、個人を特定しない範囲の情報を収集します。Cookieを無効にすることで拒否することができますので、ブラウザの設定をご確認ください。</p>
+        <h3>お客様アンケート</h3>
+        <p>当会が開催する定期演奏会では、お客様にアンケート(紙媒体)の記入をお願いしております。このアンケートでは、以下の個人情報を取得します。</p>
+        <ul>
+          <li>お名前</li>
+          <li>ご住所</li>
+        </ul>
+        <p>これらの個人情報をもとに演奏会のご案内を送らせていただきます。これ以外の用途では使用いたしません。アンケートにて記載いただいた個人情報は適切に管理し、破棄する際は第三者に漏洩がないよう適切に処理いたします。なお、保存期間は1年間としています。</p>
+        <h2>個人情報の第三者への開示・提供の禁止</h2>
+        <p>当会は、お客様よりお預かりした個人情報を適切に管理し、法令に基づき開示することが必要である場合を除き、個人情報を第三者に開示することはありません。</p>
+        <h2>個人情報の修正および削除</h2>
+        <p>お客様が、ご本人の個人情報を照会・修正・削除をご希望される場合は、当ウェブサイトのお問い合わせフォームからご依頼ください。ご本人であることを確認の上、対応させていただきます。</p>
+        <h2>お問い合わせ</h2>
+        <p>当会の個人情報の取り扱いに関するお問い合わせはフォームから受け付けております。<a href="contact">こちら</a></p>
+        <h2>プライバシーポリシーの変更について</h2>
+        <p>当会は、個人情報に関する日本の法令を遵守するとともに、本ポリシーの内容を適宜見直し、その改善に努めます。</p>
+        <p class="update">平成31年2月20日 作成</p>
+      </div>
+    </div>
+  </div>
+
+  <div class='block back-to-home'>
+    <div>
+      <a href='index.php'>ホームへ戻る</a>
+    </div>
+  </div>
+
+  <footer>
+    <div>
+      <div class='author'>
+        <a href='index.php'><?php require('image/logo/logo.svg'); ?></a>
+        <small>&copy; The Wind Ensemble 1985-<?php echo date('Y'); ?> All Rights Reserved.</small>
+      </div>
+      <div class='link'>
+        <ul>
+          <li><a href='https://member.winds-n.com'>団員専用ページ</a></li>
+          <li><a href='policy.php'>個人情報保護方針</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+  <script type="text/javascript" src="js/script.js"></script>
+</body>
+</html>
