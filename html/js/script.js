533159e354aaa7e1c91db2e96eea58d51888947f
ryo4004 <regs.ryo4004@gmail.com>
2019-05-31 00:43:31 +0900

    練習日程データへのリンクを追加

--- html/js/script.js
+++ html/js/script.js
@@ -1,2 +1,2 @@
 console.log('%c　　　　　　　　　　　', 'background: rgba(255,255,255,0) url(https://winds-n.com/img/logo.svg) no-repeat center center /contain;font-size: 32px;')
-console.log('デベロッパーツールってついつい覗いちゃうよね笑')
+console.log('お問い合わせはcontact@winds-n.comまで')
