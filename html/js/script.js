9e6ab8a0ab52f7ba975c344230eed045ef0a668f
ryo4004 <regs.ryo4004@gmail.com>
2019-08-01 01:36:18 +0900

    URL修正

--- html/js/script.js
+++ html/js/script.js
@@ -1,4 +1,4 @@
-console.log('%c　　　　　　　　　　　', 'background: rgba(255,255,255,0) url(https://winds-n.com/img/logo.svg) no-repeat center center /contain;font-size: 32px;')
+console.log('%c　　　　　　　　　　　', 'background: rgba(255,255,255,0) url(https://winds-n.com/image/logo/logo.svg) no-repeat center center /contain;font-size: 32px;')
 console.log('お問い合わせはcontact@winds-n.comまで')
 
 document.getElementById('scrollTop') ? document.getElementById('scrollTop').addEventListener('click', () => {
